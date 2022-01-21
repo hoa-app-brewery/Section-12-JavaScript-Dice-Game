8501bc630bcab636839a0db2af1bae209a6ba16b
Hoa Do <hoa.do.ma@gmail.com>
2022-01-21 01:04:45 -0500

    inital commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+
+let randomNumber1 = Math.floor((Math.random() * 6) + 1);
+let randomNumber2 = Math.floor((Math.random() * 6) + 1);
+randomNumber1;
+randomNumber2;
+
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins! 🚩";
+}
+if (randomNumber1 === randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 It's a Draw! 🚩";
+}
+if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Player 2 Wins! 🚩";
+}
